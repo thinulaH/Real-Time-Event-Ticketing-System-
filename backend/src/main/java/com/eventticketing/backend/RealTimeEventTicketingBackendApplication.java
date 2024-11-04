d45df4abdccb29fc56ab498ebdc3385bfd15dcb9
Thinula Harischandra <thinula.haris@gmail.com>
2024-11-05 00:49:43 +0530

    Initial commit with backend files

---
+++ backend/src/main/java/com/eventticketing/backend/RealTimeEventTicketingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eventticketing.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeEventTicketingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeEventTicketingBackendApplication.class, args);
+	}
+
+}
